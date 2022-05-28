91b2eeebef151b56b0ce8ff8ad1e405c429a529f
Gabriel Peres <gabriel.suporte123@gmail.com>
2022-05-28 20:05:00 -0300

    conteudo da aula 11, que foram variaveis char,inteiras,float, booleanas e algumas curiosidades

---
+++ com/gabriel/cursojava/aula11/SequenciaEscape.java
@@ -0,0 +1,13 @@
+package com.gabriel.cursojava.aula11;
+
+public class SequenciaEscape {
+
+    public static void main(String[] args) {
+        
+        // "Hello, World!"
+
+        // Barra invertida para forçar os " " a aparecem
+        System.out.println("\"Hello, World!\"");
+    }
+    
+}
---
+++ com/gabriel/cursojava/aula11/VariaveisBoolean.java
@@ -0,0 +1,21 @@
+package com.gabriel.cursojava.aula11;
+
+public class VariaveisBoolean {
+
+    public static void main(String[] args) {
+        
+        boolean verdeiro = true;
+        boolean falso = false;
+
+        System.out.println("O valor verdeiro é: " + verdeiro);
+        System.out.println("O valor falso é: " + falso);
+
+        /*if(falso == false) {
+            System.out.println("É falso mesmo, confia.");
+        } else {
+            System.out.println("Pegadinha do Mário Games");
+        }*/
+
+    }
+    
+}
